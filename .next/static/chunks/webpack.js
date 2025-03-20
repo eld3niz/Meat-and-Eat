75a953c47780da5de375c13fb41007b24ea29574
eld3niz <89333411+eld3niz@users.noreply.github.com>
2025-03-20 14:17:33 +0000

    new changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e217d0a3367f886b"; }
+/******/ 		__webpack_require__.h = function() { return "dc8ed2717af3584d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
